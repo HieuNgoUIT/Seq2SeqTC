b161c1839460b42a419b0578597860054e0e9d4a
Your Name <you@example.com>
2021-05-03 16:49:02 +0700

    using tokenized as json

--- dataset.py
+++ dataset.py
@@ -1,15 +1,14 @@
-from datasets import load_dataset
+#from datasets import load_dataset
 from transformers import BertTokenizer
 import torch
 from torch.utils.data import Dataset
-tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
-
-
+import numpy as np
+import pandas as pd
+import json
 class TCDataset(Dataset):
-    def __init__(self, src_file, trg_file, transform=None):
-        self.src_data = self.yield_data(src_file)
-        self.trg_data = self.yield_data(trg_file)
-        self.transform = transform
+    def __init__(self, src_file, trg_file):
+        self.data = self.create_data(src_file, trg_file)
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
 
     def __len__(self):
         return len(self.src_data)
@@ -17,78 +16,105 @@ class TCDataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        sample = {"src": self.src_data[idx], "trg": self.trg_data[idx]}
-        if self.transform:
-            sample = self.transform(sample)
+        sample = self.process_data_to_model_inputs(self.data.iloc[idx,:])
         return sample
 
-    def yield_data(self, path):
-        lines = []
-        with open(path, 'r') as f:
+
+
+    def create_data(self, src_file, trg_file):
+        src = []
+        trg = []
+        with open(src_file, 'r') as f:
             for line in f:
-                lines.append(line)
-        return lines                
+                src.append(line.strip())
+        with open(trg_file, 'r') as f:
+            for line in f:
+                trg.append(line.strip())
+        return pd.Series({"src" : src, "trg": trg})
+
+#dataset = TCDataset("temp_text.txt", "temp_text2.txt")
+#print(dataset[[1,2]])
 
 
+def process_data_to_model_inputs(tokenizer, src_list, trg_list):
+    # tokenize the inputs and labels
+    inputs = tokenizer(src_list, padding="max_length", truncation=True, max_length=128)
+    outputs = tokenizer(trg_list, padding="max_length", truncation=True, max_length=128)
 
-dataset = TCDataset("temp_text.txt", "temp_text2.txt")
-print(dataset[[0,1]])
+    result = {}
+    result["input_ids"] = inputs.input_ids
+    result["attention_mask"] = inputs.attention_mask
+    result["decoder_input_ids"] = outputs.input_ids
+    result["decoder_attention_mask"] = outputs.attention_mask
+    result["labels"] = outputs.input_ids.copy()
 
+    # because BERT automatically shifts the labels, the labels correspond exactly to `decoder_input_ids`.
+    # We have to make sure that the PAD token is ignored
+    result["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in
+                       result["labels"]]
+    return result
 
+def tokenize_txt_to_csv(path):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+    src = []
+    with open(path, 'r') as f:
+        for line in f:
+            src.append(line.strip())
+    trg = []
+    with open(path, 'r') as f:
+        for line in f:
+            trg.append(line.strip())
+    result = process_data_to_model_inputs(tokenizer, src, trg)
+    with open('result.json', 'w') as fp:
+        json.dump(result, fp)
+    #pd.DataFrame(result).to_csv("test.csv", index=False)
 
-
-
-
-
-
-
-
-
-
-#train_data = load_dataset('text', data_files={'train': ['temp_text.txt', 'temp_text2.txt']}, features = datasets.Features({'src'}))
-# valid_data = load_dataset('json', data_files='train.json')
-
-#print('a',train_data)
-
-# batch_size=4
-# encoder_max_length=128
-# decoder_max_length=128
-# def process_data_to_model_inputs(batch):
-#   # tokenize the inputs and labels
-#   inputs = tokenizer(batch["src"], padding="max_length", truncation=True, max_length=encoder_max_length)
-#   outputs = tokenizer(batch["trg"], padding="max_length", truncation=True, max_length=decoder_max_length)
-
-#   batch["input_ids"] = inputs.input_ids
-#   batch["attention_mask"] = inputs.attention_mask
-#   batch["decoder_input_ids"] = outputs.input_ids
-#   batch["decoder_attention_mask"] = outputs.attention_mask
-#   batch["labels"] = outputs.input_ids.copy()
-
-#   # because BERT automatically shifts the labels, the labels correspond exactly to `decoder_input_ids`. 
-#   # We have to make sure that the PAD token is ignored
-#   batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in batch["labels"]]
-
-#   return batch
+tokenize_txt_to_csv("temp_text.txt")
 
 
-# train_data = train_data.map(
-#     process_data_to_model_inputs, 
-#     batched=True, 
-#     batch_size=batch_size, 
-#     remove_columns=["src", "trg"]
+#
+# tokenizer.bos_token = tokenizer.cls_token
+# tokenizer.eos_token = tokenizer.sep_token
+# batch_size=2  # change to 16 for full training
+# encoder_max_length=512
+# decoder_max_length=512
+#
+#
+#
+# bert2bert = EncoderDecoderModel.from_encoder_decoder_pretrained('bert-base-multilingual-cased', 'bert-base-multilingual-cased') # initialize Bert2Bert from pre-trained checkpoints
+#
+#
+# # set special tokens
+# bert2bert.config.decoder_start_token_id = tokenizer.bos_token_id
+# bert2bert.config.eos_token_id = tokenizer.eos_token_id
+# bert2bert.config.pad_token_id = tokenizer.pad_token_id
+#
+# # sensible parameters for beam search
+# bert2bert.config.vocab_size = bert2bert.config.decoder.vocab_size
+# bert2bert.config.early_stopping = True
+#
+#
+# training_args = Seq2SeqTrainingArguments(
+#     output_dir="./",
+#     per_device_train_batch_size=batch_size,
+#     per_device_eval_batch_size=batch_size,
+#     predict_with_generate=True,
+#     do_train=True,
+#     do_eval=True,
+#     logging_steps=1000,  # set to 1000 for full training
+#     save_steps=500,  # set to 500 for full training
+#     eval_steps=8000,  # set to 8000 for full training
+#     warmup_steps=2000,  # set to 2000 for full training
+#     overwrite_output_dir=True,
 # )
-# train_data.set_format(
-#     type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"],
+#
+# trainer = Seq2SeqTrainer(
+#     model=bert2bert,
+#     args=training_args,
+#     #compute_metrics=metric,
+#     train_dataset=dataset,
+#     #eval_dataset=valid_data,
 # )
-
-# valid_data = train_data.map(
-#     process_data_to_model_inputs, 
-#     batched=True, 
-#     batch_size=batch_size, 
-#     #remove_columns=["src", "trg"]
-# )
-# valid_data.set_format(
-#     type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"],
-# )
-
-# print(train_data)
+#
+# trainer.train()
+#
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from transformers import EncoderDecoderModel, BertTokenizer, Seq2SeqTrainer, Seq2SeqTrainingArguments
 import torch
 from torch.utils.data import Dataset, DataLoader
-from datasets import load_dataset
+from datasets import load_dataset, Features, Value, ClassLabel, Sequence
 from transformers import BertTokenizer
 #from datasets import load_metric
 
@@ -11,83 +11,69 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
 tokenizer.bos_token = tokenizer.cls_token
 tokenizer.eos_token = tokenizer.sep_token
 
-train_data = load_dataset('json', data_files='train.json')
-valid_data = load_dataset('json', data_files='valid.json')
 
-batch_size=4  # change to 16 for full training
-encoder_max_length=512
-decoder_max_length=512
-
-def process_data_to_model_inputs(batch):
-  # tokenize the inputs and labels
-  inputs = tokenizer(batch["src"], padding="max_length", truncation=True, max_length=encoder_max_length)
-  outputs = tokenizer(batch["trg"], padding="max_length", truncation=True, max_length=decoder_max_length)
-
-  batch["input_ids"] = inputs.input_ids
-  batch["attention_mask"] = inputs.attention_mask
-  batch["decoder_input_ids"] = outputs.input_ids
-  batch["decoder_attention_mask"] = outputs.attention_mask
-  batch["labels"] = outputs.input_ids.copy()
-
-  # because BERT automatically shifts the labels, the labels correspond exactly to `decoder_input_ids`. 
-  # We have to make sure that the PAD token is ignored
-  batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in batch["labels"]]
-
-  return batch
-
-train_data = train_data.map(
-    process_data_to_model_inputs, 
-    batched=True, 
-    batch_size=batch_size, 
-    remove_columns=["src", "trg"]
+_features = Features(
+    {
+        "input_ids": Value("int64"),
+        "attention_mask": Value("int64"),
+        "decoder_input_ids": Value("int64"),
+        "decoder_attention_mask": Value("int64"),
+        "labels": Value("int64"),
+    }
 )
+
+
+
+
+train_data = load_dataset('json', data_files='result.json', split="train")#, features=_features)
+print(train_data)
+print(type(train_data))
+temp = train_data[:3]
+#valid_data = load_dataset('json', data_files='valid.json')
 train_data.set_format(
     type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"],
 )
+print(train_data)
+print(type(train_data))
 
-valid_data = valid_data.map(
-    process_data_to_model_inputs, 
-    batched=True, 
-    batch_size=batch_size, 
-    remove_columns=["src", "trg"]
-)
-valid_data.set_format(
-    type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"],
-)
+print(train_data[:3])
+print('a')
+#batch_size=4  # change to 16 for full training
+#encoder_max_length=512
+#decoder_max_length=512
 
-bert2bert = EncoderDecoderModel.from_encoder_decoder_pretrained('bert-base-multilingual-cased', 'bert-base-multilingual-cased') # initialize Bert2Bert from pre-trained checkpoints
-
-
-# set special tokens
-bert2bert.config.decoder_start_token_id = tokenizer.bos_token_id
-bert2bert.config.eos_token_id = tokenizer.eos_token_id
-bert2bert.config.pad_token_id = tokenizer.pad_token_id
-
-# sensible parameters for beam search
-bert2bert.config.vocab_size = bert2bert.config.decoder.vocab_size
-bert2bert.config.early_stopping = True
-
-
-training_args = Seq2SeqTrainingArguments(
-    output_dir="./",
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    predict_with_generate=True,
-    do_train=True,
-    do_eval=True,
-    logging_steps=1000,  # set to 1000 for full training
-    save_steps=500,  # set to 500 for full training
-    eval_steps=8000,  # set to 8000 for full training
-    warmup_steps=2000,  # set to 2000 for full training
-    overwrite_output_dir=True,
-)
-
-trainer = Seq2SeqTrainer(
-    model=bert2bert,
-    args=training_args,
-    #compute_metrics=metric,
-    train_dataset=train_data,
-    #eval_dataset=valid_data,
-)
-
-trainer.train()
+# bert2bert = EncoderDecoderModel.from_encoder_decoder_pretrained('bert-base-multilingual-cased', 'bert-base-multilingual-cased') # initialize Bert2Bert from pre-trained checkpoints
+#
+# # set special tokens
+# #bert2bert.config.decoder_start_token_id = tokenizer.bos_token_id
+# #bert2bert.config.eos_token_id = tokenizer.eos_token_id
+# #bert2bert.config.pad_token_id = tokenizer.pad_token_id
+#
+# # sensible parameters for beam search
+# #bert2bert.config.vocab_size = bert2bert.config.decoder.vocab_size
+# #bert2bert.config.early_stopping = True
+#
+#
+# # training_args = Seq2SeqTrainingArguments(
+# #     output_dir="./",
+# #     per_device_train_batch_size=batch_size,
+# #     per_device_eval_batch_size=batch_size,
+# #     predict_with_generate=True,
+# #     do_train=True,
+# #     do_eval=True,
+# #     logging_steps=1000,  # set to 1000 for full training
+# #     save_steps=500,  # set to 500 for full training
+# #     eval_steps=8000,  # set to 8000 for full training
+# #     warmup_steps=2000,  # set to 2000 for full training
+# #     overwrite_output_dir=True,
+# # )
+#
+# trainer = Seq2SeqTrainer(
+#     model=bert2bert,
+#     #args=training_args,
+#     #compute_metrics=metric,
+#     train_dataset=train_data,
+#     #eval_dataset=valid_data,
+# )
+#
+# trainer.train()
